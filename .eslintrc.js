9ec7fdff6069993c0fa1ad0437d8f6f0c25fa247
SkyLightQP <combbm@naver.com>
2020-07-03 14:05:25 +0900

    feat: Home views 추가
    chore(eslint): eslint 설정 문법 오류 수정
    fix: 필요없는 파일 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    extends: ['react-app', 'airbnb'],
-    plugins: ['react-hooks'],
-    rules: {
-      'arrow-body-style': 'off',
-      'jsx-quotes': 'off',
-      'comma-dangle': 'off',
-      'object-curly-newline': 'off',
-      'import/no-unresolved': 'off',
-      'max-len': 'off',
-      'react/jsx-indent': 'off',
-      'react/jsx-filename-extension': 'off',
-      'react/no-unescaped-entities': 'off',
-      'react/jsx-tag-spacing': 'off',
-      'react/prop-types': 'off',
-      'react/jsx-one-expression-per-line': 'off',
-      'linebreak-style': 'off'
-      'import/extensions': 'off',
-      'global-require': 'off'
-    }
+  extends: ['react-app', 'airbnb'],
+  plugins: ['react-hooks'],
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'import/no-unresolved': 'off',
+    'max-len': 'off',
+    'react/jsx-indent': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'linebreak-style': 'off',
+    'import/extensions': 'off',
+    'global-require': 'off',
+  },
 };
