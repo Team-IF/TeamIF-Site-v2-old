78b61e7cd213cda91f0ef3b708df2cb588fe413e
SkyLightQP <combbm@naver.com>
2020-08-09 14:54:29 +0900

    feat: Navigation bar, About 적용

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+import About from '../components/Sections/About';
 
 const Home: React.FC = () => {
   return (
     <>
+      <NavigationBar />
+      <About />
     </>
   );
 };
