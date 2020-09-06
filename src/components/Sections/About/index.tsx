81a743772e9cccb504ff8b068682d5d838d9c891
SkyLightQP <combbm@naver.com>
2020-09-06 23:29:48 +0900

    feat: create AlignContainer util

--- src/components/Sections/About/index.tsx
+++ src/components/Sections/About/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from '../../../atomics/Button';
 import { Heading1 } from '../../../atomics/Typography/Heading';
+import AlignContainer from '../../../utils/AlignContainer';
 
 const Container = styled.section`
   height: 26rem;
@@ -11,10 +12,6 @@ const Container = styled.section`
   align-items: center;
 `;
 
-const BodyStyle = styled.div`
-  margin-left: 20vw;
-`;
-
 const SubTitleStyle = styled.p`
   font-size: 1.3rem;
 `;
@@ -22,12 +19,12 @@ const SubTitleStyle = styled.p`
 const About: React.FC = () => {
   return (
     <Container id="about">
-      <BodyStyle>
+      <AlignContainer>
         <Heading1>We Make IF to Reality, Dream to Action.</Heading1>
         <SubTitleStyle>청소년 프로그래머 팀, TeamIF</SubTitleStyle>
 
         <Button>지원하기</Button>
-      </BodyStyle>
+      </AlignContainer>
     </Container>
   );
 };
---
+++ src/utils/AlignContainer/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const AlignContainer = styled.div`
+  width: 1500px;
+  margin: 0 auto;
+`;
+
+export default AlignContainer;
