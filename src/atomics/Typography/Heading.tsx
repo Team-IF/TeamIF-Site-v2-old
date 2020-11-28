50d7c1b324471736e6a05805c95142a1894eb4c2
combbm <combbm@naver.com>
2020-11-28 18:03:46 +0900

    Add Heading2 atomic and tip color

--- src/atomics/Typography/Heading.tsx
+++ src/atomics/Typography/Heading.tsx
@@ -1,9 +1,16 @@
 import styled from 'styled-components'
 import SCREEN_SIZE from '../../utils/ScreenSize'
 
-// eslint-disable-next-line import/prefer-default-export
 export const Heading1 = styled.p`
   font-size: 2rem;
+  font-weight: bold;
+
+  @media screen and (max-width: ${SCREEN_SIZE.MOBILE}) {
+    font-size: 1.6rem;
+  }
+`
+export const Heading2 = styled.p`
+  font-size: 1.5rem;
   font-weight: bold;
 
   @media screen and (max-width: ${SCREEN_SIZE.MOBILE}) {
