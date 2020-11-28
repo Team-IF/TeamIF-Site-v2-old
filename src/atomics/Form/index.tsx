580b7900f711ddb293163349ee7477dbcbcc4308
combbm <combbm@naver.com>
2020-11-28 18:22:58 +0900

    Create form atomic

---
+++ src/atomics/Form/index.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Label = styled.label`
+  color: var(--color-tip);
+  font-size: 12px;
+`
+
+export const Input = styled.input`
+  width: 350px;
+  height: 36px;
+
+  border: 1px solid #bdc3c7;
+  border-radius: 3px;
+
+  padding-left: 0.5rem;
+
+  font-size: 12px;
+`
+
+export const TextArea = styled.textarea`
+  width: 350px;
+  height: 200px;
+
+  border: 1px solid #bdc3c7;
+  border-radius: 3px;
+
+  padding-left: 0.5rem;
+
+  font-size: 12px;
+  font-family: 'Noto Sans KR', sans-serif;
+`
