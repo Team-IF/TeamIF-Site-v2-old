0af3ad9fa868fb93abdba992bbfab818ffb505ce
combbm <combbm@naver.com>
2020-11-28 18:04:09 +0900

    Create RecruitFormHeader component

---
+++ src/components/RecruitForm/RecruitFormHeader.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Heading2 } from '../../atomics/Typography/Heading'
+
+const TipText = styled.p`
+  color: var(--color-tip);
+  font-size: 14px;
+
+  margin-bottom: 2rem;
+`
+
+const RecruitFormHeader = () => {
+  return (
+    <div>
+      <Heading2>신청하기</Heading2>
+      <TipText>
+        디스코드는 닉네임#태그 형태가 아닌 숫자로 이루어진 ID를 입력해주세요.
+      </TipText>
+    </div>
+  )
+}
+
+export default RecruitFormHeader
