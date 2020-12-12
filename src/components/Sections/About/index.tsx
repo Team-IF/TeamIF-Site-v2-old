779e660470ed59c7a41fac65c7a8cfa4bcd8efb4
combbm <combbm@naver.com>
2020-12-12 14:30:46 +0900

    Update about section of main page

--- src/components/Sections/About/index.tsx
+++ src/components/Sections/About/index.tsx
@@ -3,18 +3,19 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import Button from '../../../atomics/Button'
 import { Heading1 } from '../../../atomics/Typography/Heading'
-import AlignContainer from '../../../utils/AlignContainer'
 
 import background from '../../../assets/background.jpg'
 
 const Container = styled.section`
-  height: 26rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  height: calc(100vh - 64px);
 
   background-image: url(${background});
   background-size: cover;
-
-  display: flex;
-  align-items: center;
 
   color: white;
 `
@@ -31,14 +32,14 @@ const ButtonStyle = styled(Button)`
 const About: React.FC = () => {
   return (
     <Container id='about'>
-      <AlignContainer>
+      <div>
         <Heading1>We Make IF to Reality, Dream to Action.</Heading1>
         <SubTitleStyle>청소년 프로그래머 팀, TeamIF</SubTitleStyle>
 
         <ButtonStyle>
           <Link to='/recruit'>지원하기</Link>
         </ButtonStyle>
-      </AlignContainer>
+      </div>
     </Container>
   )
 }
