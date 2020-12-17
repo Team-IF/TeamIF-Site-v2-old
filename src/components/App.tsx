9b28fbfbd2e810a50b30772cfadf9d3a828da45c
CheongWoli <cheong7wol@gmail.com>
2020-12-17 21:29:21 +0900

    Navbar fix and add footer bar
    Fixed #4, #5

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import NavigationBar from '../components/NavigationBar'
+import FooterBar from '../components/FooterBar'
 
 const App: React.FC = ({ children }) => {
   return (
     <>
-      <NavigationBar />
-      {children}
+      <header>
+        <NavigationBar />
+      </header>
+      <body>
+        {children}
+      </body>
+      <footer>
+        <FooterBar />
+      </footer>
     </>
   )
 }
