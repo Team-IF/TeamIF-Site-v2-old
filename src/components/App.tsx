c8b0556abb93ca484e8185fe47ae051dc9aab1a9
CheongWoli <cheong7wol@gmail.com>
2020-12-17 21:27:15 +0900

    Revert "Navbar fix and add footer bar"
    This reverts commit d46744a4881f2371893509101f873778b21107e8.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import NavigationBar from '../components/NavigationBar'
-import FooterBar from '../components/FooterBar'
 
 const App: React.FC = ({ children }) => {
   return (
     <>
-      <header>
-        <NavigationBar />
-      </header>
-      <body>
-        {children}
-      </body>
-      <footer>
-        <FooterBar />
-      </footer>
+      <NavigationBar />
+      {children}
     </>
   )
 }
